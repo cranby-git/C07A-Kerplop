0eae75cb69cb4c983cbd0f69580190eb94654f0b
Amelia Bell <amelia.bell.school@gmail.com>
2025-09-29 10:46:45 -0600

    Added the movement test for Lobster -Amelia

--- src/tests/TestMovingPieces.java
+++ src/tests/TestMovingPieces.java
@@ -1,15 +1,18 @@
 package tests;
 
 import org.junit.jupiter.api.Assertions.*;
+import org.junit.Assert;
+import org.junit.Test;
 
 import levelPieces.*;
 import gameEngine.GameEngine;
 import gameEngine.Drawable;
 
 public class TestMovingPieces {
-/*
- * Test that the ghost goes back and forth properly with obstacles to hide behind
-*/
+	/*
+	 * Test that the ghost goes back and forth properly with obstacles to hide behind
+	*/
+	@Test
 	public void testGhost() {
 		// set up board and ghost
 		Drawable[] gameBoard = new Drawable[GameEngine.BOARD_SIZE];
@@ -28,5 +31,40 @@ public class TestMovingPieces {
 			assert(gameBoard[10] != testGhost);
 		}
 		assert(gameBoard[GameEngine.BOARD_SIZE - 1] == testGhost);
+	}
+	
+	/*
+	 * 
+	 */
+	@Test
+	public void testLobster() {
+		// set up test environment
+		Drawable[] gameBoard = new Drawable[GameEngine.BOARD_SIZE];
+		Lobster testLobster = new Lobster('L', "testLobster", 1);
+		gameBoard[1] = testLobster;
+		
+		// add obstacles (other pieces)
+		gameBoard[5] = new Tesla('T', "teslaObstacle", 5);
+		gameBoard[10] = new MudPit(10);
+		
+		// create array for all spaces to test for random movement
+		Boolean testBoolArray[] = new Boolean[GameEngine.BOARD_SIZE];
+		for(int i = 0; i < 100; i++) {
+			testLobster.move(gameBoard, 0);
+			// set true to show that a space has been visited
+			testBoolArray[testLobster.getLocation()] = true;
+		}
+		
+		// check array 
+		for(int i = 0; i < GameEngine.BOARD_SIZE - 1; i++) {
+			//should never teleport on other objects
+			if (i == 5 || i == 10) {
+				Assert.assertEquals(false, testBoolArray[i]);
+				continue;
+			}
+			//should have traveled to every other spot
+			Assert.assertEquals(true, testBoolArray[i]);
+		}
+		
 	}
 }
